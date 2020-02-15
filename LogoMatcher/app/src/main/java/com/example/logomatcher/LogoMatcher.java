70cf456599e175fe3d9e77c07eb69a4fdbdb41a1
kapilkhandelwal <61082416+kapilkhandelwal@users.noreply.github.com>
2020-02-15 17:56:56 +0530

    Update LogoMatcher.java

--- LogoMatcher/app/src/main/java/com/example/logomatcher/LogoMatcher.java
+++ LogoMatcher/app/src/main/java/com/example/logomatcher/LogoMatcher.java
@@ -1,25 +1,50 @@
 package com.example.logomatcher;
 
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.logomatcher.Logo;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class LogoMatcher {
-    private ArrayList<Logo> logos;
-    private final String jsonString = "FINAL.json";
+    private int currentLogo = 0;
+    ArrayList<TextView> textViewArray;
+    ArrayList<TextView> answerTextViewArray;
+    ImageView imageView;
+    JSONArray jsonArray;
 
-    LogoMatcher() {
-        logos = parseJSON();
+    LogoMatcher(ArrayList<TextView> textViewArray, ArrayList<TextView> answerTextViewArray, ImageView imageView) {
+        this.textViewArray = textViewArray;
+        this.answerTextViewArray = answerTextViewArray;
+        this.imageView = imageView;
+        try {
+            jsonArray = new JSONArray("JSON STRING"); //TODO: Parse JSON file to string
+        } catch(JSONException e) {
+
+        }
+
+        parseElementAndDisplayUI();
     }
 
-    private ArrayList<Logo> parseJSON() {
-        ArrayList<Logo> arLogo = new ArrayList<Logo>();
-        JSONObject jsonObj;
-        return arLogo;
+    private void parseElementAndDisplayUI() {
+        try {
+            JSONObject jsonObject = jsonArray.getJSONObject(currentLogo);
+             new Logo(imageView, jsonObject.getString("name"),
+                     textViewArray, answerTextViewArray).execute(jsonObject.getString("imgUrl"));
+        } catch (JSONException e) {
+        }
+        return ;
+    }
+
+    public void next() {
+        currentLogo++;
+        parseElementAndDisplayUI();
+
     }
 
 
