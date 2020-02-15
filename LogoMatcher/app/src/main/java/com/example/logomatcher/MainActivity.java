41b69b3da77c6b9c6332aa041afb13c22fdd01ee
kapilkhandelwal <61082416+kapilkhandelwal@users.noreply.github.com>
2020-02-15 17:56:17 +0530

    Update MainActivity.java

--- LogoMatcher/app/src/main/java/com/example/logomatcher/MainActivity.java
+++ LogoMatcher/app/src/main/java/com/example/logomatcher/MainActivity.java
@@ -10,10 +10,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    TextView mTv1, mTv2, mTv3, mTv4, mTv5, mTv6, mTv7, mTv8, mTv9;
     ImageView mImageView;
-    TextView mATV1, mATV2, mATV3, mATV4, mATV5, mATV6, mATV7, mATV8, mATV9;
+    LogoMatcher mMatcher;
+    ArrayList<TextView> tvArray;
+    ArrayList<TextView> answerTVArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +30,11 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                mMatcher.next();
             }
         });
-        mTv1 = findViewById(R.id.textView);
-        mTv2 = findViewById(R.id.textView2);
-        mTv3 = findViewById(R.id.textView3);
-        mTv4 = findViewById(R.id.textView4);
-        mTv5 = findViewById(R.id.textView5);
-        mTv6 = findViewById(R.id.textView6);
-        mTv7 = findViewById(R.id.textView7);
-        mTv8 = findViewById(R.id.textView8);
-        mTv9 = findViewById(R.id.textView9);
-        mATV1 = findViewById(R.id.answerTV);
-        mATV2 = findViewById(R.id.answerTV2);
-        mATV3 = findViewById(R.id.answerTV3);
-        mATV4 = findViewById(R.id.answerTV4);
-        mATV5 = findViewById(R.id.answerTV5);
-        mATV6 = findViewById(R.id.answerTV6);
-        mATV7 = findViewById(R.id.answerTV7);
-        mATV8 = findViewById(R.id.answerTV8);
-        mATV9 = findViewById(R.id.answerTV9);
 
-
-
-//        new Logo((ImageView) findViewById(R.id.imageView))
-//                .execute(MY_URL_STRING);
+        mMatcher = new LogoMatcher(tvArray, answerTVArray, mImageView);
     }
 
 
@@ -65,8 +49,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
     public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    private void populateTextViews() {
+        tvArray = new ArrayList<TextView>();
+        tvArray.add((TextView)findViewById(R.id.textView));
+        tvArray.add((TextView)findViewById(R.id.textView2));
+        tvArray.add((TextView)findViewById(R.id.textView3));
+        tvArray.add((TextView)findViewById(R.id.textView4));
+        tvArray.add((TextView)findViewById(R.id.textView5));
+        tvArray.add((TextView)findViewById(R.id.textView6));
+        tvArray.add((TextView)findViewById(R.id.textView7));
+        tvArray.add((TextView)findViewById(R.id.textView8));
+        tvArray.add((TextView)findViewById(R.id.textView9));
+
+        answerTVArray.add((TextView)findViewById(R.id.answerTV));
+        answerTVArray.add((TextView)findViewById(R.id.answerTV2));
+        answerTVArray.add((TextView)findViewById(R.id.answerTV3));
+        answerTVArray.add((TextView)findViewById(R.id.answerTV4));
+        answerTVArray.add((TextView)findViewById(R.id.answerTV5));
+        answerTVArray.add((TextView)findViewById(R.id.answerTV6));
+        answerTVArray.add((TextView)findViewById(R.id.answerTV7));
+        answerTVArray.add((TextView)findViewById(R.id.answerTV8));
+        answerTVArray.add((TextView)findViewById(R.id.answerTV9));
     }
 
 
