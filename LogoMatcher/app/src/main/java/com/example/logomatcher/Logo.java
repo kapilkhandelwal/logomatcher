c0a5032b9d1ad9b854f7d07768776c3febf8e47d
kapilkhandelwal <61082416+kapilkhandelwal@users.noreply.github.com>
2020-02-15 17:57:34 +0530

    Update Logo.java

--- LogoMatcher/app/src/main/java/com/example/logomatcher/Logo.java
+++ LogoMatcher/app/src/main/java/com/example/logomatcher/Logo.java
@@ -5,19 +5,31 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Logo extends AsyncTask<String, Void, Bitmap>{
 
     String mActualName;
     ImageView bmImage;
+    String shuffledName;
+    String url;
+    ArrayList<TextView> tvArray;
+    ArrayList<TextView> answerTVArray;
 
 
 
-    public Logo(ImageView bmImage, String url, String actualName) {
+    public Logo(ImageView bmImage, String actualName, ArrayList<TextView> tvArray, ArrayList<TextView> answerTVArray) {
         this.bmImage = bmImage;
-        execute(url);
+        this.mActualName = actualName;
+        shuffledName = shuffleString(actualName);
+        this.tvArray = tvArray;
+        this.answerTVArray = answerTVArray;
     }
 
     protected Bitmap doInBackground(String... urls) {
@@ -35,4 +47,17 @@ public class Logo extends AsyncTask<String, Void, Bitmap>{
         bmImage.setImageBitmap(result);
     }
 
+    public  String shuffleString(String string)
+    {
+        List<String> letters = Arrays.asList(string.split(""));
+        Collections.shuffle(letters);
+        String shuffled = "";
+        for (String letter : letters) {
+            shuffled += letter;
+        }
+        return shuffled;
+    }
+
+    //TODO: Put shuffled string one-by-one to tvArray views and set Visilble
+    //TODO: Put actualName string one by one to answerTVArray, keep invisible and make them visible on onClick of tcArray text views.
 }
